00c2f11c054d8c254bbc9a575d09e32672bc8aec
Babalpreet <babaldon1111@gmail.com>
2024-04-25 23:15:56 +0530

    final project
    final

--- myblog/src/app/featured-post/featured-post.component.ts
+++ myblog/src/app/featured-post/featured-post.component.ts
@@ -15,14 +15,14 @@ interface FeaturedPost {
 export class FeaturedPostComponent {
   featuredPosts: FeaturedPost[] = [
     {
-      title: 'Featured Post 1',
-      imageUrl: 'https://example.com/image1.jpg',
-      description: 'Description for Featured Post 1'
+      title: 'Marine Life',
+      imageUrl: 'https://imgs.search.brave.com/uMGg_r2XjnSSdK7JoLlFOPeN88FLW6N5X-zdn_paIFM/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvOTgy/NTQzNjYyL3Bob3Rv/L2JlYXV0aWZ1bC1m/aXNoLWZpc2guanBn/P3M9NjEyeDYxMiZ3/PTAmaz0yMCZjPWZF/a1pxdXdGSkExbjFL/QTZKNHkxbTl2a3Rh/NC1QeV9ISDl3OWl2/endWNEU9',
+      description: 'Explore the enigmatic cosmic phenomena where gravity is so strong that nothing, not even light, can escape.'
     },
     {
-      title: 'Featured Post 2',
-      imageUrl: 'https://example.com/image2.jpg',
-      description: 'Description for Featured Post 2'
+      title: 'Black Holes',
+      imageUrl: 'https://imgs.search.brave.com/8N4_O-gCOz7Usfw2BsomEgbl3HDqkFp1dsInKi8iTt0/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/YnJpdGFubmljYS5j/b20vNTcvMTgxODU3/LTA1MC05NzJBNEY5/Ni9BcnRpc3QtcmVu/ZGVyaW5nLW1hdHRl/ci1ibGFjay1ob2xl/LmpwZz93PTMwMA',
+      description: ': Dive into the vibrant and diverse world beneath the ocean surface, teeming with fascinating creatures and ecosystems.'
     },
     // Add more featured posts as needed
   ];
